7a103e96361b1bf2184ffa391e505a91302a957b
Patricia O'Brien <patriciaobrien@wireless-10-145-124-99.public.utexas.edu>
2015-10-15 23:19:41 -0500

    no message

--- 02 Data Wrangling/DataWrangling3.R
+++ 02 Data Wrangling/DataWrangling3.R
@@ -8,4 +8,5 @@ edu <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?qu
 
 emp <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from UNEMPLOYMENT"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_pjo293', PASS='orcl_pjo293', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
-dplyr::left_join(emp, edu, by = "FIPS_CODE") %>% View
+dplyr::left_join(emp, edu, by = "FIPS_CODE") %>% group_by(URBAN_INFLUENCE_CODE2013, UNEMPLOYMENT_RATE_2013) %>% filter(URBAN_INFLUENCE_CODE2013 %in% c("1","2","3","4","5","6","7","8","9","10","11","12")) %>% ggplot(aes(x = as.numeric(as.character(UNEMPLOYMENT_RATE_2013)), y = as.numeric(as.character(HS_DIPLOMA_ONLY2009_13))), color = URBAN_INFLUENCE_CODE2013) + labs(x="Unemployment Rate", y=paste("Number of People Who Have High School Diplomas Only"))+ geom_point()
+
