d8c367b54937d782a5c51f1379be43335843df43
Patricia O'Brien <patriciaobrien@wireless-10-145-124-99.public.utexas.edu>
2015-10-15 21:17:06 -0500

    no message

--- 00 Doc/Education_ETL.R
+++ 00 Doc/Education_ETL.R
@@ -1,17 +1,15 @@
 setwd("/Users/patriciaobrien/DataVisualization/DV_RProject3/01 Data")
-
-file_path <- "test.csv"
-
+file_path <- "Education.csv"
 df <- read.csv(file_path, stringsAsFactors = FALSE)
-
 # Replace "." (i.e., period) with "_" in the column names.
 names(df) <- gsub("\\.+", "_", names(df))
 
-str(df) # Uncomment this and  run just the lines to here to get column types to use for getting the list of measures.
+#str(df) # Uncomment this and  run just the lines to here to get column types to use for getting the list of measures.
 
 summary(df)
+head(df)
 
-measures <- c("Less_than_a_HS_diploma_1970", "HS_diploma_only_1970", "Some_college_1_3_years_1970", "X4_years_of_college_or_more_1970", "Less_than_a_HS_diploma_1980", "HS_diploma_only_1980", "Some_college_1_3_years_1980", "X4_years_of_college_or_more_1980", "Less_than_a_HS_diploma_1990", "HS_diploma_only_1990", "Some_college_or_associate_s_degree_1990", "Bachelor_s_degree_or_more_1990", "Less_than_a_HS_diploma_2000", "HS_diploma_only_2000", "Some_college_or_associate_s_degree_2000", "Bachelor_s_degree_or_more_2000", "Less_than_a_HS_diploma_09_13", "HS_diploma_only_09_13", "Some_college_or_associate_s_degree_09_13", "Bachelor_s_degree_or_more_09_13")
+measures <- c("Less_than_HS_Diploma1970", "HS_Diploma_Only1970", "Some_College1970", "Four_Yrs_College1970", "Less_than_HS_Diploma1980", "HS_Diploma_Only1980", "Some_College1980", "Four_Yrs_College1980", "Less_than_HS_Diploma1990", "HS_Diploma_Only1990", "Some_College1990", "Bachelors1990", "Less_than_HS_Diploma2000", "HS_Diploma_Only2000", "Some_College2000", "Bachelors2000", "Less_than_HS_Diploma2009_13", "HS_Diploma_Only2009_13", "Some_College2009_13", "Bachelors2009_13")
 
 #measures <- NA # Do this if there are no measures.
 
@@ -36,8 +34,8 @@ if( length(measures) > 1 || ! is.na(dimensions)) {
 library(lubridate)
 # Fix date columns, this needs to be done by hand because | needs to be correct.
 #                                                        \_/
-df$Order_Date <- gsub(" [0-9]+:.*", "", gsub(" UTC", "", mdy(as.character(df$Order_Date), tz="UTC")))
-df$Ship_Date  <- gsub(" [0-9]+:.*", "", gsub(" UTC", "", mdy(as.character(df$Ship_Date),  tz="UTC")))
+#df$Order_Date <- gsub(" [0-9]+:.*", "", gsub(" UTC", "", mdy(as.character(df$Order_Date), tz="UTC")))
+#df$Ship_Date  <- gsub(" [0-9]+:.*", "", gsub(" UTC", "", mdy(as.character(df$Ship_Date),  tz="UTC")))
 
 # The following is an example of dealing with special cases like making state abbreviations be all upper case.
 # df["State"] <- data.frame(lapply(df["State"], toupper))
